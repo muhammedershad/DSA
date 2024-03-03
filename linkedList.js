d90e1102f74be50292084d934be97a8c69701d09
Muhammed Ershad P <muhammedershadp@gmail.com>
2024-03-03 21:59:25 +0530

    linked list

---
+++ linkedList.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class LL {
+    constructor (head) {
+        head = this.head
+        size = 0
+    }
+}
